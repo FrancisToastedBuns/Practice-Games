defca0f87afdd60ccbf4f610c026c6f4f05d78d5
Filipino Bread <Mesa.Francis.Allen.S@gmail.com>
2025-05-24 21:49:32 +0800

    fixed bullet bug

--- space-rocks/assets/player/bullets/bullet.gd
+++ space-rocks/assets/player/bullets/bullet.gd
@@ -16,9 +16,3 @@ func _on_area_entered(area):
 	if area.is_in_group("enemy"):
 		area.take_damage(1)
 		queue_free()
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("rocks"):
-		body.explode()
-		queue_free()
