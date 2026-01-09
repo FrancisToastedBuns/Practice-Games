ab1b3789f7cc16d77939fd9e33c1d0f976658092
Filipino Bread <Mesa.Francis.Allen.S@gmail.com>
2026-01-09 10:30:00 +0800

    added timeout on bullets

--- space_rocks/assets/player/player.gd
+++ space_rocks/assets/player/player.gd
@@ -1,8 +1,22 @@
 extends RigidBody2D
 @export var Bullet : PackedScene
+var recently_shot = true
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_pressed("shoot"):
+		print("testing")
+		if recently_shot:
+			shoot()
+			recently_shot = false
+			await get_tree().create_timer(0.3).timeout
+			recently_shot = true
+	if Input.is_action_just_pressed("rotate_left"):
+		$P
+	if Input.is_action_just_pressed("rotate_right"):
+		shoot()
+	if Input.is_action_just_pressed("thrust"):
+		shoot()
+	if Input.is_action_just_pressed("pause"):
 		shoot()
 
 func shoot():
