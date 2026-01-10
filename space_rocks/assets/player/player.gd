bc236b50081cc43820513ae64e82001838be32c9
Filipino Bread <Mesa.Francis.Allen.S@gmail.com>
2026-01-10 10:30:00 +0800

    Update player.gd

--- space_rocks/assets/player/player.gd
+++ space_rocks/assets/player/player.gd
@@ -1,25 +1,42 @@
 extends RigidBody2D
 @export var Bullet : PackedScene
 var recently_shot = true
+var rotation_speed = 8000
+var spinning = 0
+var thrust = Vector2.ZERO
+var engine_power = 300
 
-func _physics_process(delta):
+var rotation_dir = 0
+var screensize = Vector2.ZERO
+var can_shoot = true
+var reset_pos = false
+
+func _physics_process(_delta):
+	constant_torque = rotation_speed * spinning
+	constant_force = thrust
+	screensize = get_viewport_rect().size
+
+func _process(_delta: float) -> void:
+	thrust = Vector2.ZERO
+	spinning = Input.get_axis("rotate_left","rotate_right")
+	if Input.is_action_pressed("thrust"):
+		thrust = transform.x * engine_power
 	if Input.is_action_pressed("shoot"):
-		print("testing")
-		if recently_shot:
+		if can_shoot:
 			shoot()
-			recently_shot = false
+			can_shoot = false
 			await get_tree().create_timer(0.3).timeout
-			recently_shot = true
-	if Input.is_action_just_pressed("rotate_left"):
-		$P
-	if Input.is_action_just_pressed("rotate_right"):
-		shoot()
-	if Input.is_action_just_pressed("thrust"):
-		shoot()
-	if Input.is_action_just_pressed("pause"):
-		shoot()
-
+			can_shoot = true
 func shoot():
 	var b = Bullet.instantiate()
 	add_child(b)
 	b.transform = $Muzzle.transform
+
+func _integrate_forces(physics_state):
+	if reset_pos:
+		physics_state.transform.origin = screensize / 2
+		reset_pos = false
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
