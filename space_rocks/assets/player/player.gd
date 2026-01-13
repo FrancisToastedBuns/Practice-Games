e8898f200ac242f2094274d063855a3ae85108f3
Filipino Bread <Mesa.Francis.Allen.S@gmail.com>
2026-01-13 10:30:00 +0800

    added states and some music

--- space_rocks/assets/player/player.gd
+++ space_rocks/assets/player/player.gd
@@ -1,5 +1,10 @@
 extends RigidBody2D
 @export var Bullet : PackedScene
+@export var explosion_sound : Resource
+
+enum {INIT, ALIVE, INVULNERABLE, DEAD}
+var player_state
+
 var recently_shot = true
 var rotation_speed = 8000
 var spinning = 0
@@ -11,12 +16,21 @@ var screensize = Vector2.ZERO
 var can_shoot = true
 var reset_pos = false
 
+
+func _ready() -> void:
+	player_state = DEAD
+
 func _physics_process(_delta):
 	constant_torque = rotation_speed * spinning
 	constant_force = thrust
 	screensize = get_viewport_rect().size
 
 func _process(_delta: float) -> void:
+	get_input()
+	
+func get_input():
+	if player_state == DEAD:
+		return
 	thrust = Vector2.ZERO
 	spinning = Input.get_axis("rotate_left","rotate_right")
 	if Input.is_action_pressed("thrust"):
@@ -31,6 +45,7 @@ func shoot():
 	var b = Bullet.instantiate()
 	get_tree().root.add_child(b)
 	b.start($Muzzle.global_transform)
+	$BulletSound.play()
 func _integrate_forces(physics_state):
 	if reset_pos:
 		physics_state.transform.origin = screensize / 2
--- space_rocks/main.gd
+++ space_rocks/main.gd
@@ -1,1 +1,5 @@
 extends Node
+
+func _process(_delta: float) -> void:
+	if not $Music.playing:
+		$Music.play()
