23bbdd0b56de1415d44b889e60f088547fc08034
Filipino Bread <Mesa.Francis.Allen.S@gmail.com>
2026-01-14 12:58:41 +0800

    added start button

--- space_rocks/assets/ui/HUD.gd
+++ space_rocks/assets/ui/HUD.gd
@@ -1,1 +1,7 @@
 extends CanvasLayer
+signal start
+
+func _on_start_button_pressed() -> void:
+	$MarginContainer.show()
+	$VBoxContainer.hide()
+	start.emit()
--- space_rocks/main.gd
+++ space_rocks/main.gd
@@ -1,5 +1,15 @@
 extends Node
-
+var playing = false
 func _process(_delta: float) -> void:
-	if not $Music.playing:
+	if not $Music.playing and playing:
 		$Music.play()
+	elif not $Music.playing:
+		return
+
+func _ready() -> void:
+	$Player.hide()
+
+func _on_hud_start() -> void:
+	playing = true
+	$Player.show()
+	$Player.player_state = ALIVE
