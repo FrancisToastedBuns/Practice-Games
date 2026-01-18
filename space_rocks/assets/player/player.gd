71d0d85d8fa8018024ee3c1237206e1b857e1808
Filipino Bread <Mesa.Francis.Allen.S@gmail.com>
2026-01-18 10:30:00 +0800

    Update player.gd

--- space_rocks/assets/player/player.gd
+++ space_rocks/assets/player/player.gd
@@ -1,56 +1,97 @@
 extends RigidBody2D
-@export var Bullet : PackedScene
-@export var explosion_sound : Resource
+@export var max_shield = 100.0
+@export var shield_regen = 5.0
+@export var engine_power = 500
+@export var spin_power = 8000
+@export var bullet_scene : PackedScene
+@export var fire_rate = 0.25
+
+
+
+signal lives_changed
+signal dead
+signal shield_changed
 
 enum {INIT, ALIVE, INVULNERABLE, DEAD}
-var player_state
 
-var recently_shot = true
-var rotation_speed = 8000
-var spinning = 0
+var shield = 0: set = set_shield
+var state = INIT
 var thrust = Vector2.ZERO
-var engine_power = 300
-
 var rotation_dir = 0
 var screensize = Vector2.ZERO
 var can_shoot = true
 var reset_pos = false
+var lives = 0: set = set_lives
 
-
-func _ready() -> void:
-	player_state = DEAD
-
-func _physics_process(_delta):
-	constant_torque = rotation_speed * spinning
-	constant_force = thrust
+func set_lives(value):
+	lives = value
+	lives_changed.emit(lives)
+	shield = max_shield
+	if lives <= 0:
+		change_state(DEAD)
+	else:
+		change_state(INVULNERABLE)
+func _ready():
+	change_state(ALIVE)
 	screensize = get_viewport_rect().size
 
-func _process(_delta: float) -> void:
-	get_input()
-	
-func get_input():
-	$CPUParticles2D.hide()
-	if player_state == DEAD:
-		return
-	thrust = Vector2.ZERO
-	spinning = Input.get_axis("rotate_left","rotate_right")
-	if Input.is_action_pressed("thrust"):
-		thrust = transform.x * engine_power
-		$CPUParticles2D.show()
-	if Input.is_action_pressed("shoot"):
-		if can_shoot:
-			shoot()
-			can_shoot = false
-			await get_tree().create_timer(0.15).timeout
-			can_shoot = true
+func change_state(new_state):
+	match new_state:
+		INIT:
+			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
+		ALIVE:
+			$CollisionShape2D.set_deferred("disabled", false)
+			$Sprite2D.modulate.a = 1.0
+		INVULNERABLE:
+			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
+			$InvulnerabilityTimer.start()
+		DEAD:
+			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.hide()
+			linear_velocity = Vector2.ZERO
+			$EngineSounds.stop()
+			dead.emit()
+	state = new_state
 
-func player_reset():
-	player_state = ALIVE
+func _process(delta):
+	shield += shield_regen * delta
+	get_input()
+
+func get_input():
+	thrust = Vector2.ZERO
+	if state in [DEAD, INIT]:
+		return
+	if Input.is_action_pressed("thrust"):
+		$Exhaust.emitting = true
+		thrust = transform.x * engine_power
+		if not $EngineSounds.playing:
+			$EngineSounds.play()
+	else:
+		$EngineSounds.stop()
+		$Exhaust.emitting = false
+	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
+
 func shoot():
-	var b = Bullet.instantiate()
+	if state == INVULNERABLE:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
 	get_tree().root.add_child(b)
 	b.start($Muzzle.global_transform)
-	$BulletSound.play()
+	$LaserSound.play()
+	
+func _on_gun_cooldown_timeout():
+	can_shoot = true
+	
+func _physics_process(_delta):
+	constant_force = thrust
+	constant_torque = rotation_dir * spin_power
+
 func _integrate_forces(physics_state):
 	if reset_pos:
 		physics_state.transform.origin = screensize / 2
@@ -59,3 +100,30 @@ func _integrate_forces(physics_state):
 	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
 	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
 	physics_state.transform = xform
+
+func _on_invulnerability_timer_timeout():
+	change_state(ALIVE)
+
+func reset():
+	reset_pos = true
+	$Sprite2D.show()
+	lives = 3
+	change_state(ALIVE)
+
+func _on_body_entered(body):
+	if body.is_in_group("rocks"):
+		shield -= body.size * 25
+		body.explode()
+func explode():
+	$Explosion.show()
+	$Explosion/AnimationPlayer.play("explosion")
+	await $Explosion/AnimationPlayer.animation_finished
+	$Explosion.hide()
+	$ExplosionSound.play()
+func set_shield(value):
+	value = min(value, max_shield)
+	shield = value
+	shield_changed.emit(shield / max_shield)
+	if shield <= 0:
+		lives -= 1
+		explode()
